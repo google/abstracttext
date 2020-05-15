3883d15e84567de7ca8f03969934c753e55b0b63
Arthur Smith <arthurpsmith@gmail.com>
2020-05-15 09:59:12 -0400

    Add "test now" button in UI

--- resources/ext.abstractText/index.js
+++ resources/ext.abstractText/index.js
@@ -1,4 +1,32 @@
-const addTestResults = (element, results) => {
+const runATest = (event) => {
+  let target = $( event.target );
+  let zid = event.data.zid;
+  let testid = event.data.testid;
+  let impl = event.data.impl;
+  let parent = target.parent();
+  let reportArea = parent.find( "span" );
+  if ( reportArea.length === 0 ) {
+    reportArea = $('<span>');
+    parent.append(reportArea);
+  }
+  reportArea.html("... waiting for results ...");
+
+  $.get("/api.php?action=abstracttext&function=" + zid + "&runATest=1&testId=" + testid + "&implementationId=" + impl + "&format=json",
+    function( data ) {
+      const results = data.testResults;
+      reportArea.html("Result: " + results.result + " (expected " + results.expected + ") " + results.ms + "ms");
+    });
+}
+
+const runButton = (zid, testid, impl) => {
+  let buttonElement = $('<button>');
+  buttonElement.attr('type', 'button');
+  buttonElement.click({zid: zid, testid: testid, impl: impl}, runATest);
+  buttonElement.html('Test Now');
+  return buttonElement;
+}
+
+const addTestResults = (element, results, zid) => {
   let testid = element.attr('data-testid');
   let testResults = results[testid];
   element.append($('<strong>').html('Test results by implementation:'));
@@ -11,7 +39,8 @@ const addTestResults = (element, results) => {
     errors = implInfo['errors'];
     time = implInfo['average_ms'];
     inner.html('Average ' + time + 'ms in ' + run_count + ' runs (' + errors + ' errors)');
-    row.append(inner)
+    row.append(inner);
+    row.append($('<td>').append(runButton(zid, testid, impl)));
     outer.append(row);
   }
   element.append(outer);
@@ -25,7 +54,7 @@ $( function() {
       const results = data.abstracttext;
       if (results !== null) {
         $( '.test_result' ).each(function() {
-           addTestResults($( this ), results);
+           addTestResults($( this ), results, zid);
        });
       }
     });
