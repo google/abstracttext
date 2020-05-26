ab36fd2e3ac300c7784d879bf8cd29e8ed10720e
Arthur Smith <arthurpsmith@gmail.com>
2020-05-26 11:11:35 -0400

    Basic db query and display functionality

--- includes/SpecialObjectsByType.php
+++ includes/SpecialObjectsByType.php
@@ -2,19 +2,32 @@
 
 namespace AbstractText;
 
-use SpecialPage;
+use AbstractText\AbstractTextSpecialPage ;
 
-class SpecialObjectsByType extends SpecialPage {
+class SpecialObjectsByType extends AbstractTextSpecialPage {
 	function __construct() {
 		parent::__construct( 'ObjectsByType' );
 	}
 
 	function execute( $par ) {
-		$request = $this->getRequest();
 		$output = $this->getOutput();
+		$dbr = wfGetDB( DB_REPLICA );
+
+		$types_list = $this->getTypes($dbr);
 
 		$this->setHeaders();
-		$wikitext = '== List of objects by type ==';
+		$wikitext = "== Available types ==\n";
+		foreach ($types_list AS $type) {
+			$wikitext .= "# [[Special:ObjectsByType/$type|$type]]\n";
+		}
+		if ( $par !== null ) {
+			$wikitext .= "\n== List of objects of type $par ==\n";
+
+			$zobjects_list = $this->getObjectsByType($par, $dbr);
+			foreach ($zobjects_list AS $zobject) {
+				$wikitext .= "# [[M:$zobject|$zobject]]\n";
+			}
+		}
 		$output->addWikiTextAsInterface( $wikitext );
 	}
 }
