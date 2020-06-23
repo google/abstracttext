36bd2dbbab5bb9d12cc7bef94e206c0cacb59a14
Arthur Smith <arthurpsmith@gmail.com>
2020-06-23 12:10:00 -0400

    docker support for running with master Mediawiki branch

--- docker_support/AdditionalLocalSettings.php
+++ docker_support/AdditionalLocalSettings.php
@@ -3,3 +3,4 @@ include_once 'DockerLocalSettings.php';
 $wgCacheEpoch = max( $wgCacheEpoch, gmdate( 'YmdHis' ) );
 wfLoadExtension( 'UniversalLanguageSelector' );
 wfLoadExtension( 'AbstractText' );
+wfLoadSkin( 'Vector' );
